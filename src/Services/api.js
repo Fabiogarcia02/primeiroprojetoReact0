9fb4f0dd31debac4303268d92b51000e85084185
Fábio Martins <fabiogarciamartins2025@gmail.com>
2025-10-29 18:01:51 -0300

    Inicio da consumação de api

---
+++ src/Services/api.js
@@ -0,0 +1,3 @@
+// base da url da API: https://api.themoviedb.org/3/
+
+//Url da api de filmes mais populares: https://api.themoviedb.org/3/movie/now_playing?api_key=d9bace77444306ce3d18fc8f8b400725&language=pt-BR&page=1
